0268a99c58c5e620f71be8137dfb1687aa8bf371
Ricardo Fabio Caterino Polini <ricardocaterino@gmail.com>
2023-03-01 19:23:06 +0100

    improve component reading

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -1,9 +1,11 @@
-import { NoFavorites } from '@/components/ui';
+import { useEffect, useState } from 'react';
+
+import { NoFavorites } from '../../components/ui';
 
 import { Layout } from '../../components/layouts';
-import { useEffect, useState } from 'react';
-import { localFavorites } from '@/utils';
-import { Card, Grid } from '@nextui-org/react';
+
+import { localFavorites } from '../../utils';
+import { FavoritePokemons } from '../../components/pokemon';
 
 const FavoritesPage = () => {
 
@@ -14,43 +16,13 @@ const FavoritesPage = () => {
 
   }, [])
 
-
-
   return (
     <Layout title='Pokémons - Favoritos'>
-
       {
         favoritePokemons.length === 0
         ? ( <NoFavorites /> )
-        : (
-
-          <Grid.Container gap={ 2 } direction='row' justify='flex-start'>
-            {
-              favoritePokemons.map( id => (
-                <Grid xs={ 6 } sm={ 3 } md={ 2 } xl={ 1 } key={ id }>
-                  <Card isHoverable isPressable css={{ padding: 10 }}>
-                    <Card.Image 
-                    src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ id }.svg`}
-                    width={'100%'}
-                    height={140}
-                    
-                    />
-                    
-                  </Card>
-
-
-                </Grid>
-              ) )
-            }
-
-
-          </Grid.Container>
-        )
-
-
+        : ( <FavoritePokemons pokemons={ favoritePokemons } /> )
       }
-
-
     </Layout>
   )
 };
